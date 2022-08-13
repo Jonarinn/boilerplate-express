060a9bf0ba7427cbd328797aeb13d259fcae91a4
Jonarinn <105772027+Jonarinn@users.noreply.github.com>
2022-08-13 15:11:57 +0000

    Finished some challenges
    Finished 50% of the challenges

--- myApp.js
+++ myApp.js
@@ -1,11 +1,28 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
 
 
+console.log('Hello World');
 
+app.get('/', function(req, res) {
+    res.sendFile( __dirname + "/views/index.html")
+});
 
+app.use('/public', express.static(__dirname + '/public'));
 
+app.get('/json', ( req, res ) => {
+    const helloJson = 'Hello json'
+    
+    const message = process.env.MESSAGE_STYLE === 'uppercase' ? helloJson.toUpperCase() : helloJson;
+    const data = {
+        'message': message
+    }
+    
+    res.json(data)
+    
+})
 
 
 
